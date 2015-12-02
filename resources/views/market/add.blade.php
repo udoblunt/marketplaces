cd86c399586f1ae68fa7440e8f37712b3915d4ef
Steven Grauwmans <s.grauwmans@gmail.com>
2015-12-02 17:42:05 +0100

    updated controllers (markets + loggedIn)

--- resources/views/market/add.blade.php
+++ resources/views/market/add.blade.php
@@ -4,26 +4,12 @@
 	
 @section('title', $title)
 
-@section('marketplaces')
-    @parent
-
-	    @foreach ($markets as $market)
-			<a class="pull-left" href="{{ url('/m', [$market->name]) }}">{{ $market->name }}</a>
-		@endforeach
-@endsection
-
-@section('sidebar')
-    @parent
-    
-@endsection
-
 @section('content')
 	<h1>Add a new market</h1>
 	
         <form id="addMarket" method="post" action="{{ url('ctrl/m/add') }}"> <!-- addMarket Form -->
             
             {!! csrf_field() !!}
-            
             <div> <!-- name Div -->
                 <label for="name">Name</label>
                 <input form="addMarket" type="text" name="name" placeholder="Name of your market, e.g. Bikes" required="">
@@ -31,7 +17,7 @@
             
             <div> <!-- description Div -->
                 <label for="description">Description</label>
-                <textarea form="addMarket" name="description" cols="5" rows="10" placeholder="Add a description for your market, e.g. all sorts of bikes (including tricycle)" required=""></textarea>
+                <textarea form="addMarket" name="description" cols="20" rows="10" placeholder="Add a description for your market, e.g. all sorts of bikes (including tricycle)" required=""></textarea>
             </div> <!-- /description Div -->
             
             <div> <!-- defaultAttributes Div -->
