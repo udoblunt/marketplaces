46d864f7812cb443d7e9199611a6b21fa686f3b2
Steven Grauwmans <s.grauwmans@gmail.com>
2015-12-02 16:23:47 +0100

    removed /home from routes, added the add_market view

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -41,15 +41,19 @@ class ComposerAutoloaderInit251136b198e48907f3a401f493fcc739
         $loader->register(true);
 
         $includeFiles = require __DIR__ . '/autoload_files.php';
-        foreach ($includeFiles as $file) {
-            composerRequire251136b198e48907f3a401f493fcc739($file);
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire251136b198e48907f3a401f493fcc739($fileIdentifier, $file);
         }
 
         return $loader;
     }
 }
 
-function composerRequire251136b198e48907f3a401f493fcc739($file)
+function composerRequire251136b198e48907f3a401f493fcc739($fileIdentifier, $file)
 {
-    require $file;
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+    }
 }
